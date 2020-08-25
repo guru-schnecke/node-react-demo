91d51f3abb8f33762d0d0d917e3fb51c944237a0
Schnecke <eiweala@live.com>
2020-08-25 14:31:53 +0800

    1st api call

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 //======= require all dependencies
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 //=== add all middlewares
 require("./config/db"); //calls my mongoose connection to cleanup this file
 app.use(express.json()); //allows me to receive JSON files from HEADER of REQUEST
+app.use(cors()); //allows all requests from outside servers or apps
 //=== setup my routes
 app.use("/api/items", require("./routes/item.route"));
 
--- running/src/App.js
+++ running/src/App.js
@@ -4,12 +4,40 @@ import Home from "./component/Home";
 import Item from "./component/items/Item";
 import Register from "./component/auth/Register";
 import Login from "./component/auth/Login";
+import Navigation from "./component/Navigation";
+import Axios from "axios";
+
+const URL = process.env.REACT_APP_URL;
 export default class App extends Component {
+  state = {
+    items: [],
+  };
+
+  fetchItems = () => {
+    Axios.get(`${URL}/items`)
+      .then((res) => {
+        // console.log(res.data);
+        this.setState({ items: res.data.items });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  componentDidMount() {
+    this.fetchItems();
+  }
+
   render() {
     return (
       <Router>
+        <Navigation />
         <Switch>
-          <Route path="/" exact render={() => <Home />} />
+          <Route
+            path="/"
+            exact
+            render={() => <Home items={this.state.items} />}
+          />
           <Route path="/item/:id" exact render={() => <Item />} />
           <Route path="/register" exact render={() => <Register />} />
           <Route path="/login" exact render={() => <Login />} />
